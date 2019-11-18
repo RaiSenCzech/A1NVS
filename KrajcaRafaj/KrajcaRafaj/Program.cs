d8c27ce32090324fc84b910cd2b02f2ab8dce8af
RaiSenCzech <50031296+RaiSenCzech@users.noreply.github.com>
2019-11-18 12:24:52 +0100

    Add files via upload

---
+++ KrajcaRafaj/KrajcaRafaj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KrajcaRafaj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public double[] matice = new double[] {1,2,3,4,5,6};
+
+        }
+    }
+}
